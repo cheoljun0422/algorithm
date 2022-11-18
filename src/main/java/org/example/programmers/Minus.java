9c6f6b1f7a3fdbd77a173fad8f2b53674c1cd5fc
cheoljun <ljc3484@naver.com>
2022-11-18 16:41:36 +0900

    문제 설명 추가

--- src/main/java/org/example/programmers/Minus.java
+++ src/main/java/org/example/programmers/Minus.java
@@ -1,12 +1,14 @@
 package org.example.programmers;
-
+/*
+정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록
+soltuion 함수를 완성해주세요.
+ */
 class Minus {
     public int solution(int num1, int num2) {
         int answer;
         answer = num1 - num2;
         return answer;
     }
-
     public static void main(String[] args) {
         Minus n = new Minus();
 
